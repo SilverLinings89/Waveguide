e8b75a1c4a14ca32716fc9aa32e4e18fc9080291
Pascal Kraft <kraft.pascal@gmail.com>
2016-06-03 11:18:20 +0200

    umfpack -> mumps

--- Waveguide.cpp
+++ Waveguide.cpp
@@ -1811,7 +1811,8 @@ void Waveguide<TrilinosWrappers::SparseMatrix, TrilinosWrappers::MPI::Vector >::
 
 			// TrilinosWrappers::PreconditionBlockwiseDirect bd;
 			// bd.initialize(prec_matrix, TrilinosWrappers::PreconditionBlockwiseDirect::AdditionalData(7000));
-			TrilinosWrappers::SolverDirect prec_sol(solver_control, TrilinosWrappers::SolverDirect::AdditionalData(true, "Amesos_Umfpack"));
+			TrilinosWrappers::SolverDirect prec_sol(solver_control, TrilinosWrappers::SolverDirect::AdditionalData(true, "Amesos_Mumps"));
+
 			PreconditionerSweeping sweep( &prec_sol, prec_matrix, locally_owned_dofs.n_elements(), below);
 			std::cout << GlobalParams.MPI_Rank << " ready to solve" <<std::endl;
 			// solver.solve(system_matrix,solution, system_rhs, sweep);
