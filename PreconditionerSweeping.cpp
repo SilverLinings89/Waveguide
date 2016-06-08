bc4b030335208602a2215bb79ec1b9b27e91e52d
Pascal Kraft <kraft.pascal@gmail.com>
2016-06-08 15:36:12 +0200

    include changed

--- PreconditionerSweeping.cpp
+++ PreconditionerSweeping.cpp
@@ -5,6 +5,7 @@
 #include <deal.II/lac/vector.templates.h>
 #include <deal.II/lac/trilinos_vector.h>
 #include <deal.II/lac/trilinos_vector_base.h>
+#include <deal.II/lac/solver.h>
 
 #include "PreconditionerSweeping.h"
 
