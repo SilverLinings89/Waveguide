c40fd8f5bd80c3fb4f2d6fd2195e364075675d76
Pascal Kraft <pascal.kraft@kit.edu>
2017-04-04 19:29:19 +0200

    Major bugfix in preconditioner

--- Code/Core/PreconditionerSweeping.cpp
+++ Code/Core/PreconditionerSweeping.cpp
@@ -20,7 +20,7 @@ PreconditionerSweeping::~PreconditionerSweeping (){
   delete solver;
 }
 
-PreconditionerSweeping::PreconditionerSweeping (  MPI_Comm in_mpi_comm, int in_own, int in_others, int in_bandwidth, IndexSet in_locally_owned_dofs, IndexSet * in_fixed_dofs, int in_rank)
+PreconditionerSweeping::PreconditionerSweeping (  MPI_Comm in_mpi_comm, int in_own, int in_others, int in_above, int in_bandwidth, IndexSet in_locally_owned_dofs, IndexSet * in_fixed_dofs, int in_rank)
 {
     locally_owned_dofs = in_locally_owned_dofs;
 		own = in_own;
@@ -36,7 +36,7 @@ PreconditionerSweeping::PreconditionerSweeping (  MPI_Comm in_mpi_comm, int in_o
 		rank = in_rank;
 		bandwidth = in_bandwidth;
 		mpi_comm = in_mpi_comm;
-
+		above = in_above;
 }
 
 
@@ -57,42 +57,27 @@ void PreconditionerSweeping::vmult (TrilinosWrappers::MPI::BlockVector       &ds
 	}
 
 	if ((int)rank+1 == GlobalParams.NumberProcesses) {
-
-		solver->solve( input);
-
+	  solver->solve( input);
 		MPI_Send(&input[0], own, MPI_DOUBLE, rank-1, 0, mpi_comm);
-
 	} else {
-
-
-		double * trans2 = new double[others];
+	  double * trans2 = new double[others];
 		MPI_Recv(trans2, others, MPI_DOUBLE, rank+1, 0, mpi_comm, MPI_STATUS_IGNORE);
-        
 		dealii::Vector<double> temp2 (others);
 		for (int i = 0; i < others; i++) {
 			temp2[i] = trans2[i];
 		}
-
-		dealii::Vector<double> temp3;
-		temp3.reinit(own, false);
-
-
-		LowerProduct(temp2, temp3);
-
+		dealii::Vector<double> temp3 (own);
+		UpperProduct(temp2, temp3);
 		input -= temp3;
-		
 		if(rank != 0) {
 			dealii::Vector<double> temp4 (own);
-
 			Hinv(input, temp4);
-
 			MPI_Send(&temp4[0], own, MPI_DOUBLE, rank - 1, 0, mpi_comm);
 		}
 
 	}
         
   MPI_Barrier(mpi_comm);
-
   if((int)rank +1 != GlobalParams.NumberProcesses) {
       dealii::Vector<double> temp (own);
       for(int i =0; i < own; i++) {
@@ -102,29 +87,25 @@ void PreconditionerSweeping::vmult (TrilinosWrappers::MPI::BlockVector       &ds
   }
 
   MPI_Barrier(mpi_comm);
-
   if ( rank == 0) {
-      dealii::Vector<double> back_sweep (others);
-      UpperProduct(input, back_sweep);
-      MPI_Send(&back_sweep[0], others, MPI_DOUBLE, rank + 1, 0, mpi_comm);
-
+      MPI_Send(&input[0], own, MPI_DOUBLE, rank + 1, 0, mpi_comm);
   } else {
-      double * trans4 = new double [own];
-      MPI_Recv(trans4, own, MPI_DOUBLE, rank-1, 0, mpi_comm, MPI_STATUS_IGNORE);
-
+      double * trans4 = new double [above];
+      MPI_Recv(trans4, above, MPI_DOUBLE, rank-1, 0, mpi_comm, MPI_STATUS_IGNORE);
       dealii::Vector<double> back_sweep (own);
-      dealii::Vector<double> temp_calc (own);
+      dealii::Vector<double> back_sweep2 (own);
+      dealii::Vector<double> temp_calc (above);
       for (int i = 0; i < own; i++) {
               temp_calc[i] = trans4[i];
       }
-
-      Hinv(temp_calc, back_sweep);
-      input -= back_sweep;
+      LowerProduct(temp_calc, back_sweep);
+      Hinv(back_sweep, back_sweep2);
+      input -= back_sweep2;
 
       if((int)rank +1< GlobalParams.NumberProcesses) {
-          dealii::Vector<double> back_sweep2 (others);
-          UpperProduct(input, back_sweep2);
-          MPI_Send(&back_sweep2[0], others, MPI_DOUBLE, rank + 1, 0, mpi_comm);
+
+          MPI_Send(&input[0], own, MPI_DOUBLE, rank + 1, 0, mpi_comm);
+
       }
   }
 
@@ -133,6 +114,7 @@ void PreconditionerSweeping::vmult (TrilinosWrappers::MPI::BlockVector       &ds
       dst[indices[i]] = input[i];
     }
   }
+
 
 }
 
@@ -154,27 +136,18 @@ void PreconditionerSweeping::Hinv(const dealii::Vector<double> & src, dealii::Ve
 	}
 }
 
-void PreconditionerSweeping::LowerProduct(const dealii::Vector<double> & src, dealii::Vector<double> & dst) const {
 
-  if((int)rank +1 == GlobalParams.NumberProcesses) {
-		std::cout << "ERROR!" <<std::endl;
-	}
+void PreconditionerSweeping::init(SolverControl , TrilinosWrappers::SparseMatrix * in_prec_upper, TrilinosWrappers::SparseMatrix * in_prec_lower) {
 
-	prec_matrix_lower->vmult(dst, src);
-
-}
-
-void PreconditionerSweeping::init(SolverControl , TrilinosWrappers::SparseMatrix * in_prec) {
-
+  deallog.push("Init Preconditioner");
+  deallog << "Prepare Objects" <<std::endl;
   solver = new SparseDirectUMFPACK();
   IndexSet local (matrix->m());
   local.add_range(0, matrix->m());
 
+  // Main Matrix Preparation
+
   sparsity_pattern.reinit(own+ others, own+others, bandwidth);
-
-  off_diag_block.reinit(own, others, bandwidth);
-
-
 
   TrilinosWrappers::SparseMatrix::iterator it = matrix->begin();
   TrilinosWrappers::SparseMatrix::iterator end = matrix->end();
@@ -185,35 +158,58 @@ void PreconditionerSweeping::init(SolverControl , TrilinosWrappers::SparseMatrix
 
   sparsity_pattern.compress();
 
-  if(others != 0){
-    it = in_prec->begin();
-    end = in_prec->end();
-    for(; it != end; it++){
-      off_diag_block.add(it->row(), it->column());
-    }
-  } else {
-    off_diag_block.reinit(own,own, 120);
-    it = in_prec->begin();
-    end = in_prec->end();
-    for(; it != end; it++){
-      off_diag_block.add(it->row(), it->column());
-    }
-  }
-  off_diag_block.compress();
-
   temp = new dealii::SparseMatrix<double>(sparsity_pattern);
-
+  deallog << "Copy Matrix" <<std::endl;
   temp->copy_from(* matrix);
-
+  deallog << "Factorize Matrix" <<std::endl;
   solver->factorize(*temp);
 
-  IndexSet ownindeces(own);
-  ownindeces.add_range(0, own);
+  // Prec Matrix lower Preparation
+  deallog << "Prepare Lower Block" <<std::endl;
+  off_diag_block_lower.reinit(own, above, bandwidth);
 
-  prec_matrix_lower = new dealii::SparseMatrix<double>(off_diag_block);
-  prec_matrix_lower->copy_from(*in_prec);
+  if(above != 0){
+    it = in_prec_lower->begin();
+    end = in_prec_lower->end();
+    for(; it != end; it++){
+      off_diag_block_lower.add(it->row(), it->column());
+    }
+  } else {
+    off_diag_block_lower.reinit(own,own, 120);
+    it = in_prec_lower->begin();
+    end = in_prec_lower->end();
+    for(; it != end; it++){
+      off_diag_block_lower.add(it->row(), it->column());
+    }
+  }
+  off_diag_block_lower.compress();
 
+  prec_matrix_lower = new dealii::SparseMatrix<double>(off_diag_block_lower);
+  prec_matrix_lower->copy_from(*in_prec_lower);
 
+  // Prec Matrix upper Preparation
+  deallog << "Prepare Upper Block" <<std::endl;
+  off_diag_block_upper.reinit(own, others, bandwidth);
+
+  if(others != 0){
+    it = in_prec_upper->begin();
+    end = in_prec_upper->end();
+    for(; it != end; it++){
+      off_diag_block_upper.add(it->row(), it->column());
+    }
+  } else {
+    off_diag_block_upper.reinit(own,own, 120);
+    it = in_prec_upper->begin();
+    end = in_prec_upper->end();
+    for(; it != end; it++){
+      off_diag_block_upper.add(it->row(), it->column());
+    }
+  }
+  off_diag_block_upper.compress();
+
+  prec_matrix_upper = new dealii::SparseMatrix<double>(off_diag_block_upper);
+  prec_matrix_upper->copy_from(*in_prec_upper);
+  deallog.pop();
 }
 
 void PreconditionerSweeping::UpperProduct(const dealii::Vector<double> & src, dealii::Vector<double> & dst) const {
@@ -223,7 +219,17 @@ void PreconditionerSweeping::UpperProduct(const dealii::Vector<double> & src, de
 		std::cout << "ERROR!" <<std::endl;
 	}
 
-	prec_matrix_lower->Tvmult(dst, src);
+	prec_matrix_upper->vmult(dst, src);
+
+}
+
+void PreconditionerSweeping::LowerProduct(const dealii::Vector<double> & src, dealii::Vector<double> & dst) const {
+
+  if((int)rank  == 0) {
+    std::cout << "ERROR!" <<std::endl;
+  }
+
+  prec_matrix_lower->vmult(dst, src);
 
 }
 
