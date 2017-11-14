fc2b5d4c8230d69e94f8254c52a5c69a2f2dda38
Dein Name <deine@emailadresse.de>
2017-11-14 17:59:38 +0100

    Some new execute scripts.

--- job_96.sh
+++ job_96.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 module load mpi/openmpi/2.0-intel-16.0
 module load lib/hdf5/1.8-openmpi-2.0-intel-16.0
-mpirun --bind-to core --map-by core -report-bindings ./main
+mpirun --bind-to core --map-by core -report-bindings ./main Parameters/Parameters_96mm.xml
