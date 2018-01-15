7524abd1636a4cd89d273f496de1f7b093fcd26d
Dein Name <deine@emailadresse.de>
2018-01-15 15:19:37 +0100

    Ein paar neue Files fuer den Großrechner

--- task_runner.sh
+++
@@ -1,8 +0,0 @@
- #!/bin/bash
- 
- counter=1
- while [ $counter -lt 1025 ]; do\
-	temp="0$(bc<<<"scale=5;$counter/10000")"
-	mpirun -np 4 ./main Parameters/Parameters.xml $temp
-	counter=$((counter *2)) 
-done
