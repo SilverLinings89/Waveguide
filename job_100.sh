511c788061c4b3e30aa2f36228ce6b7efce3eec7
Dein Name <deine@emailadresse.de>
2018-03-05 16:41:56 +0100

    s_100

---
+++ job_100.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+module load mpi/openmpi/2.0-intel-16.0
+module load lib/hdf5/1.8-openmpi-2.0-intel-16.0
+mpirun --bind-to core --map-by core -report-bindings ./Main/main Parameters/PWB_Case_5.xml
