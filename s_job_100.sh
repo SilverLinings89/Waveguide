1dd6c6811459b168d8184e851832897dab80c3e2
kraft <kraft.pascal@gmail.com>
2018-03-05 16:44:36 +0100

    Cleanup

--- s_job_100.sh
+++ s_job_100.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 module load mpi/openmpi/2.0-intel-16.0
 module load lib/hdf5/1.8-openmpi-2.0-intel-16.0
-mpirun --bind-to core --map-by core -report-bindings ./main Parameters/Straight_Rectangular_100.xml
+mpirun --bind-to core --map-by core -report-bindings ./main Parameters/Rectangular/Straight/100.xml
