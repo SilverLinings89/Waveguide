2725637520875fe970dbe0637e86db614c922399
Dein Name <deine@emailadresse.de>
2018-03-12 23:40:43 +0100

    Anpassung des Plotskripts für neuen Dateinamen

--- make_phase_plot.sh
+++ make_phase_plot.sh
@@ -5,4 +5,4 @@ set output 'powerplot.png'
 set key autotitle columnheader
 set style data lines
 set xlabel 'z in \mu m'
-plot 'complex qualities.dat' using 1:2, '' using 1:3, '' using 1:4" | gnuplot
+plot 'complex_qualities.dat' using 1:2, '' using 1:3, '' using 1:4" | gnuplot
