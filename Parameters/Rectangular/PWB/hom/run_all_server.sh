c9dffca4ddf4221c222975c4e06c5b6d0823e482
Pascal Kraft <kraft.pascal@gmail.com>
2019-01-04 14:50:25 +0100

    Another bugfix about automated mass computations.

--- Parameters/Rectangular/PWB/hom/run_all_server.sh
+++ Parameters/Rectangular/PWB/hom/run_all_server.sh
@@ -3,13 +3,13 @@ StepWidth=1
 val=35 #The first computed Case is val + StepWidth and Zerobased so the line computed from the shapelist is linenumber = val+stepwidth-1
 SaveVal=${val}
 NumberOfCases=21
-for value in {1..$NumberOfCases}
+for value in $(seq 1 $NumberOfCases)
 do 
 	val=$(expr $val + $StepWidth)
 	rm ./case_${val}.sh
 	rm Parameters/Rectangular/PWB/hom/PWB_Case_${val}.xml
-	cp Parameters/Rectangular/PWB/hom/PWB_Case_base.xml Parameters/Rectangular/PWB/hom/PWB_Case_${val}.xml
-	sed "s/#CASENUMBER#/${val}/g" Parameters/Rectangular/PWB/hom/PWB_Case_${val}.xml
+	touch Parameters/Rectangular/PWB/hom/PWB_Case_${val}.xml
+	sed "s/#CASENUMBER#/${val}/g" Parameters/Rectangular/PWB/hom/PWB_Case_${val}.xml >> Parameters/Rectangular/PWB/hom/PWB_Case_${val}.xml
 	echo "#!/bin/bash" >> ./case_${val}.sh
 	echo "module load lib/boost/1.56.0" >> ./case_${val}.sh
 	echo "module load compiler/gnu/5.2" >> ./case_${val}.sh
@@ -21,7 +21,7 @@ do
 done
 
 val=${SaveVal}
-for value in {1..$NumberOfCases}
+for value in $(seq 1 $NumberOfCases)
 do 
 	val=$(expr $val + $StepWidth)
 	echo msub -q multinode -l nodes=4:ppn=8,pmem=6000mb,walltime=04:00:00 case_${val}.sh
