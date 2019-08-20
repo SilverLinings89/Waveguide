8bfc6dd66306edac40fdc84c2c8afef2ed473ce5
Pascal Kraft <pascal.kraft@kit.edu>
2019-08-20 17:06:51 +0200

    Work on HSIE dof counting etc.

--- Code/HSIEPreconditioner/HSIESurface.cpp
+++ Code/HSIEPreconditioner/HSIESurface.cpp
@@ -2,4 +2,80 @@
 // Created by kraft on 16.08.19.
 //
 
+#include <deal.II/grid/grid_generator.h>
 #include "HSIESurface.h"
+
+template<unsigned int ORDER>
+void HSIESurface<ORDER>::prepare_surface_triangulation() {
+    dealii::GridGenerator::extract_boundary_mesh(main_triangulation, surface_triangulation, b_id);
+}
+
+template<unsigned int ORDER>
+HSIESurface<ORDER>::HSIESurface(dealii::Triangulation<3, 3> *in_main_triangulation, unsigned int in_boundary_id,
+                                unsigned int in_level):
+                                main_triangulation(in_main_triangulation),
+                                b_id(in_boundary_id),
+                                level(in_level) {
+
+}
+
+template<unsigned int ORDER>
+void HSIESurface<ORDER>::compute_dof_numbers() {
+    this->n_edge_dofs = compute_n_edge_dofs();
+    this->n_face_dofs = compute_n_face_dofs();
+    this->n_vertex_dofs = compute_n_vertex_dofs();
+}
+
+template<unsigned int ORDER>
+void HSIESurface<ORDER>::fill_matrix(dealii::SparseMatrix<double> *, dealii::IndexSet) {
+
+}
+
+template<unsigned int ORDER>
+unsigned int HSIESurface<ORDER>::get_n_own_hsie_dofs() {
+    return n_vertex_dofs.owned_hsie + n_edge_dofs.owned_hsie + n_face_dofs.owned_hsie;
+}
+
+template<unsigned int ORDER>
+DofCount HSIESurface<ORDER>::compute_n_edge_dofs() {
+    DofCount ret;
+    return ret;
+}
+
+template<unsigned int ORDER>
+DofCount HSIESurface<ORDER>::compute_n_vertex_dofs() {
+    DofCount ret;
+    return ret;
+}
+
+template<unsigned int ORDER>
+DofCount HSIESurface<ORDER>::compute_n_face_dofs() {
+    DofCount ret;
+    return ret;
+}
+
+template<unsigned int ORDER>
+unsigned int HSIESurface<ORDER>::compute_dofs_per_edge(bool only_edgge_dofs) {
+    return 0;
+}
+
+template<unsigned int ORDER>
+unsigned int HSIESurface<ORDER>::compute_dofs_per_face(bool only_face_dofs) {
+    return 0;
+}
+
+template<unsigned int ORDER>
+unsigned int HSIESurface<ORDER>::compute_dofs_per_vertex() {
+    return 0;
+}
+
+template<unsigned int ORDER>
+void HSIESurface<ORDER>::initialize() {
+    initialize_dof_handlers();
+    compute_dof_numbers();
+}
+
+template<unsigned int ORDER>
+void HSIESurface<ORDER>::initialize_dof_handlers() {
+
+}
--- Code/HSIEPreconditioner/HSIESurface.h
+++ Code/HSIEPreconditioner/HSIESurface.h
@@ -7,6 +7,10 @@
 
 #include <deal.II/grid/tria.h>
 
+struct DofCount {
+    unsigned int owned, non_owned,  hsie, non_hsie, owned_hsie, total;
+};
+
 template<unsigned int ORDER>
 class HSIESurface {
     unsigned int n_total_hsie_dofs;
@@ -16,15 +20,29 @@ class HSIESurface {
     unsigned int n_pure_hsie_dofs;
     const unsigned int b_id;
     unsigned int level;
+    DofCount n_vertex_dofs, n_face_dofs, n_edge_dofs;
 
 public:
     HSIESurface(dealii::Triangulation<3,3> * in_main_triangulation, unsigned int in_boundary_id, unsigned int in_level);
+
     void prepare_surface_triangulation();
     void compute_dof_numbers();
     void fill_matrix(dealii::SparseMatrix<double>* , dealii::IndexSet);
-    unsigned int get_n_own_dofs();
+    unsigned int get_n_own_hsie_dofs();
+    DofCount compute_n_edge_dofs();
+    DofCount compute_n_vertex_dofs();
+    DofCount compute_n_face_dofs();
+    unsigned int compute_dofs_per_edge(bool only_edgge_dofs);
+    unsigned int compute_dofs_per_face(bool only_face_dofs);
+    unsigned int compute_dofs_per_vertex();
+    void initialize();
+    void initialize_dof_handlers();
+
+
 
 };
+
+
 
 
 #endif //WAVEGUIDEPROBLEM_HSIESURFACE_H
