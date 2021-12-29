1516b4db9b79c1724ea90d2bcb8fe47ebfe13a09
Pascal Kraft <kraft.pascal@gmail.com>
2021-12-30 00:17:02 +0100

    Loading default modules instead of specific module.

--- build/job_ompi_35.sh
+++ build/job_ompi_35.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Use when a defined module environment related to OpenMPI is wished
-module load mpi/openmpi/4.0
+module restore
 mpirun --bind-to core --map-by core -report-bindings Main/main --run ../Parameters/Run/3-3-25.prm --case ../Parameters/Case/hump_examples/predefined_case_35_with_pml.prm
