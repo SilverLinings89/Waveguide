00d6cb5639350b49c87c7cb8afa7695e4d23a762
Pascal Kraft <kraft.pascal@gmail.com>
2021-12-30 18:58:48 +0100

    Another experiment for dof numbering validation.

--- Code/BoundaryCondition/BoundaryCondition.cpp
+++ Code/BoundaryCondition/BoundaryCondition.cpp
@@ -168,21 +168,32 @@ void BoundaryCondition::force_validation() {
           std::vector<InterfaceDofData> d = get_dof_association_by_boundary_id(surf);
      //     std::cout << "B" << std::endl;
           bool one_is_invalid = false;
+          unsigned int count_before = 0;
+          unsigned int count_after = 0;
           for(unsigned int index = 0; index < d.size(); index++) {
             if(!is_dof_owned[d[index].index]) {
               if(global_index_mapping[d[index].index] >= Geometry.levels[level].n_total_level_dofs) {
                 one_is_invalid = true;
+                count_before ++;
               }
             }
           }
        //   std::cout << "C" << std::endl;
           if(one_is_invalid) {
             std::cout << "Forcing validation on " << b_id << " for " << surf << std::endl;
-            std::vector<unsigned int> local_indices;
+            std::vector<unsigned int> local_indices(d.size());
             for(unsigned int i = 0; i < d.size(); i++) {
-              local_indices.push_back(d[i].index);
+              local_indices[i] = d[i].index;
             }
             set_non_local_dof_indices(local_indices, Geometry.levels[level].surfaces[surf]->get_global_dof_indices_by_boundary_id(b_id));
+            for(unsigned int index = 0; index < d.size(); index++) {
+              if(!is_dof_owned[d[index].index]) {
+                if(global_index_mapping[d[index].index] >= Geometry.levels[level].n_total_level_dofs) {
+                  count_after ++;
+                }
+              }
+            }
+            std::cout << "Count before: " << count_before << " and after: " << count_after << std::endl;
           }
         }
     }
