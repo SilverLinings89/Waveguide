080ef8837be38ed9b5026666158936c470060edd
Pascal Kraft <kraft.pascal@gmail.com>
2022-02-21 23:15:47 -0800

    Another minor update to parameter files.

--- run/job_hump_23.sh
+++ run/job_hump_23.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Use when a defined module environment related to OpenMPI is wished
 module restore
-mpiexec.hydra -bootstrap slurm ../build/Main/main --run ../Parameters/Run/3-3-24.prm --case ../Parameters/Case/hump_examples/predefined_case_23_with_pml.prm
+mpiexec.hydra -bootstrap slurm ../build/Main/main --run ../Parameters/Run/3-3-39.prm --case ../Parameters/Case/hump_examples/predefined_case_23_with_pml.prm
