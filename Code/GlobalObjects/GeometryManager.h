ee47810f7df475b38e3128f99714810ae72fc642
Pascal Kraft <kraft.pascal@gmail.com>
2022-03-23 01:49:58 +0100

    Some more comments.

--- Code/GlobalObjects/GeometryManager.h
+++ Code/GlobalObjects/GeometryManager.h
@@ -72,25 +72,113 @@ class GeometryManager {
 
   virtual ~GeometryManager();
 
+  /**
+   * @brief Parent of the entire initialization loop
+   * This initializes all levels of the computation.
+   */
   void initialize();
+
+  /**
+   * @brief On the level in_level this builds the InnerDomain object
+   * 
+   * @param in_level The level to perform the action on.
+   */
   void initialize_inner_domain(unsigned int in_level);
-  void initialize_surfaces();
-  void perform_initialization(unsigned int level);
+
+  /**
+   * @brief This function computes the term epsilon_r * omega^2 at a given location. 
+   * This is required for the assembly of the Maxwell system.
+   * 
+   * @return double \epsilon_r * \omega^2
+   */
   double eps_kappa_2(Position);
+
+  /**
+   * @brief Like the function above but without epsilon_r.
+   * Since this value is independent of the position, this function has no arguments.
+   * @return double \omega^2
+   */
   double kappa_2();
 
+  /**
+   * @brief Computes the range of the coordinate x this process is responsible for.
+   * Since the local domains are always of the form [min_x, max_x]\times[min_y, max_y]\times[min_z, max_z], these ranges can be used to describe the local problem.
+   * @return std::pair<double, double> first is the lower bound of the range, second is the upper bound.
+   */
   std::pair<double, double> compute_x_range();
+
+  /**
+   * @brief Same as above but for y.
+   * 
+   * @return std::pair<double, double> see above. 
+   */
   std::pair<double, double> compute_y_range();
+
+  /**
+   * @brief Same as above but for z.
+   * 
+   * @return std::pair<double, double> see above.
+   */
   std::pair<double, double> compute_z_range();
 
-  void set_x_range(std::pair<double, double>);
-  void set_y_range(std::pair<double, double>);
-  void set_z_range(std::pair<double, double>);
+  /**
+   * @brief Fixes the x-range this process is working on for its inner domain.
+   * Boundary conditions can extend beyond this value however. The idea is to use the return value of compute_x_range().
+   * @param inp_x the x_range to use locally.
+   */
+  void set_x_range(std::pair<double, double> inp_x);
+  
+  /**
+   * @brief Fixes the y-range this process is working on for its inner domain.
+   * Boundary conditions can extend beyond this value however. The idea is to use the return value of compute_y_range().
+   * @param inp_y the y_range to use locally.
+   */
+  void set_y_range(std::pair<double, double> inp_y);
 
-  std::pair<bool, unsigned int> get_global_neighbor_for_interface(Direction);
-  std::pair<bool, unsigned int> get_level_neighbor_for_interface(Direction, unsigned int);
+  /**
+   * @brief Fixes the z-range this process is working on for its inner domain.
+   * Boundary conditions can extend beyond this value however. The idea is to use the return value of compute_z_range().
+   * @param inp_z the z_range to use locally.
+   */
+  void set_z_range(std::pair<double, double> inp_z);
+
+  /**
+   * @brief For a given direction, this function computes if there is a neighbor of this process in that direction and, if so, that process's rank.
+   * 
+   * @param dir The direction to go to
+   * @return std::pair<bool, unsigned int> first: is there a process there? second: whats its rank.
+   */
+  std::pair<bool, unsigned int> get_global_neighbor_for_interface(Direction dir);
+
+  /**
+   * @brief Similar to the function above but gets the rank of the neighbor in a level communicator for the level in_level
+   * 
+   * @param dir Direction to check in
+   * @param level The level we are operating on.
+   * @return std::pair<bool, unsigned int> Same as above but second returns the rank in the level communicator.
+   */
+  std::pair<bool, unsigned int> get_level_neighbor_for_interface(Direction dir, unsigned int level);
+
+  /**
+   * @brief Checks if the coordinate is in the waveguide core or not.
+   * 
+   * @return true Location in mathematical coordinates corresponds with the interior of the waveguide.
+   * @return false it does not.
+   */
   bool math_coordinate_in_waveguide(Position) const;
+
+  /**
+   * @brief Returns a diagonalized material tensor that does not use transformation optics. Artifact.
+   * 
+   * @return dealii::Tensor<2,3> 
+   */
   dealii::Tensor<2,3> get_epsilon_tensor(const Position &);
+
+  /**
+   * @brief Computes scalar \epsilon_r for the given location.
+   * 
+   * @return double \epsilon_r of material at given location.
+   */
   double get_epsilon_for_point(const Position &);
   auto get_boundary_for_direction(Direction) -> BoundaryId;
   auto get_direction_for_boundary_id(BoundaryId) -> Direction;
