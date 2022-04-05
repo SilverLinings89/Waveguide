4650d028334290703f34faf37d7ca9d0dda2abf2
Pascal Kraft <kraft.pascal@gmail.com>
2022-04-05 12:04:59 +0200

    Adding second run to large optimization run file.

--- run/run_optimization_large.sh
+++ run/run_optimization_large.sh
@@ -7,4 +7,14 @@ mpiexec.hydra -bootstrap slurm ../build/Main/main --run ../Parameters/Run/8-8-10
 exit 0
 EOT
 
+#!/bin/bash
+
+sbatch -p multiple -n 640 --time=71:00:00 --mem=90000 --ntasks-per-node=16<<EOT
+#!/bin/bash
+module restore
+mpiexec.hydra -bootstrap slurm ../build/Main/main --run ../Parameters/Run/8-8-10max.prm --case ../Parameters/Case/optimization/larger_geometry_taper.prm
+exit 0
+EOT
+
+
 
