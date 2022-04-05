18107f5defb5d7d7783fe1807368ece13dec8db2
Pascal Kraft <kraft.pascal@gmail.com>
2022-04-05 12:01:54 +0200

    Some updates to the optimization run files.

--- Code/SpaceTransformations/WaveguideTransformation.cpp
+++ Code/SpaceTransformations/WaveguideTransformation.cpp
@@ -12,7 +12,7 @@ WaveguideTransformation::WaveguideTransformation()
     : SpaceTransformation(),
       waveguide_width(Geometry.global_z_range.first, Geometry.global_z_range.second, GlobalParams.Number_of_sectors),
       waveguide_height(Geometry.global_z_range.first, Geometry.global_z_range.second, GlobalParams.Number_of_sectors),
-      vertical_shift(Geometry.global_z_range.first, Geometry.global_z_range.second, GlobalParams.Number_of_sectors, true)      
+      vertical_shift(Geometry.global_z_range.first, Geometry.global_z_range.second, GlobalParams.Number_of_sectors, GlobalParams.Vertical_displacement_of_waveguide < 2.0)
        {
   for(unsigned int i = 0; i < 3; i++) {
     for(unsigned int j = 0; j < 3; j++) {
