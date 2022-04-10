bd3f22a0c242cefb13bc23dc90641fc69a877ed5
Pascal Kraft <kraft.pascal@gmail.com>
2022-04-10 13:27:34 +0200

    Update to optimization base cases.

--- run/run_optimization.sh
+++ run/run_optimization.sh
@@ -3,14 +3,14 @@
 sbatch -p single -n 10 --time=30:00:00 --mem=89000 <<EOT
 #!/bin/bash
 module restore
-mpiexec.hydra -bootstrap slurm ../build/Main/main --run ../Parameters/Run/10-1-1.prm --case ../Parameters/Case/optimization/larger_geometry_dirichlet.prm
+mpiexec.hydra -bootstrap slurm ../build/Main/main --run ../Parameters/Run/10-1-1.prm --case ../Parameters/Case/optimization/base.prm
 exit 0
 EOT
 
 sbatch -p single -n 10 --time=30:00:00 --mem=89000 <<EOT
 #!/bin/bash
 module restore
-mpiexec.hydra -bootstrap slurm ../build/Main/main --run ../Parameters/Run/10-1-1.prm --case ../Parameters/Case/optimization/larger_geometry_taper.prm
+mpiexec.hydra -bootstrap slurm ../build/Main/main --run ../Parameters/Run/10-1-1.prm --case ../Parameters/Case/optimization/base.prm
 exit 0
 EOT
 
