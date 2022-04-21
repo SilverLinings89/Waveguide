93b807a2c3cf93b01f7b95f2621c08495e839570
Pascal Kraft <kraft.pascal@gmail.com>
2022-04-22 01:18:26 +0200

    Minor update.

--- Code/SpaceTransformations/PredefinedShapeTransformation.h
+++ Code/SpaceTransformations/PredefinedShapeTransformation.h
@@ -11,14 +11,8 @@
 #include "SpaceTransformation.h"
 
 /**
- * \class InhomogenousTransformationRectangle
- * \brief In this case we regard a rectangular waveguide and the effects on the
- * material tensor by the space transformation and the boundary condition PML
- * may overlap (hence inhomogenous space transformation)
- *
- * If this kind of boundary condition works stably we will also be able to  deal
- * with more general settings (which might for example incorporate angles in
- * between the output and input connector. \author Pascal Kraft \date 28.11.2016
+ * \class PredefinedShapeTransformation
+ * \brief This class is used to describe the hump examples. 
  */
 
 class PredefinedShapeTransformation : public SpaceTransformation {
